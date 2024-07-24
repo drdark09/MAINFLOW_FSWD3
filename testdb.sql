2817c1ecfa0efc6daeee44658f9dd7f8ec8e8fc8
drdark09 <160234561+drdark09@users.noreply.github.com>
2024-07-24 20:42:37 +0530

    Create testdb.sql

---
+++ testdb.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL
+);
